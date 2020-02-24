5bc9542b4e7f70b6648549f64911c7dcd91d1a26
binbin.hou <binbin.hou@huifu.com>
2020-02-24 15:27:34 +0800

    [update] updates

---
+++ maven_jdk7_install.bat
@@ -0,0 +1,6 @@
+:: 用于 jdk7 的 maven clean install
+:: author: houbb
+:: LastUpdateTime:  2020-2-24 12:17:33
+:: 用法：双击运行，或者当前路径 cmd 直接输入 maven_jdk7_install.bat
+
+call mvn clean install -Dhttps.protocols=TLSv1.2
